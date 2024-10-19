c9ce26fdfb131a136c9969585d2c4d8d31f1c60d
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2024-10-19 15:26:19 -0700

    basic server setup

--- server/app.js
+++ server/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
+app.use(express.static(path.join(__dirname, '../client/')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/src/index.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });

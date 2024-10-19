d7526eebc5b40c420f46d07afca070505e0190ae
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2024-10-19 14:51:46 -0700

    added cors

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
